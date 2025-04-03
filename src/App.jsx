b231c6e5fa6953a40660d45ac6a666c39de3e00e
Kyle W. <8kwest@gmail.com>
2025-04-03 15:38:48 -0600

    docker running

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Signup from "./pages/authentication/signup";
 import Login from "./pages/authentication/login";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"; // Added Navigate import
 import Home from "./pages/home";
+import { useAuth } from "./contexts/authContext";
 
 function App() {
+  const { userLoggedIn } = useAuth();
   return (
     <Router>
       <Routes>
+        <Route path="/" element={userLoggedIn ? <Navigate to="/home" /> : <Navigate to="/login" />} />
         <Route path="login" element={<Login />}></Route>
         <Route path="signup" element={<Signup />}></Route>
         <Route path="home" element={<Home />}></Route>
