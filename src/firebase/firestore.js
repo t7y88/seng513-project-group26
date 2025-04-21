3ff6c56b6d36b80f7a1e44a6c1c1adfe215ce99d
usharabkhan <63792810+usharabkhan@users.noreply.github.com>
2025-04-21 03:11:38 -0600

    fix: added loading indicator for friends page

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -135,7 +135,6 @@ export const ensureUserExists = async (uid, authData = {}) => {
       about: "",
       description: "",
       profileImage: authData.photoURL || "",
-      admin: false,
     };
 
     // Create the user document
