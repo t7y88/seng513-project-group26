1421c09a844fe0c80f1463fd786023a8d19f79ff
NoshinZion <noshin.zion@ucalgary.ca>
2025-04-22 00:31:26 -0600

    Updated the useAuth.js file

--- src/contexts/authContext/useAuth.js
+++ src/contexts/authContext/useAuth.js
@@ -1,8 +1,32 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { defaultAuthContext } from "../../types/types";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../firebase";
 
 const AuthContext = React.createContext(defaultAuthContext);
 
 export function useAuth() {
   return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setUserLoggedIn(!!user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, userLoggedIn, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
