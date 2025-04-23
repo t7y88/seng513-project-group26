728f323e5d6c3bac5a74e2f7fd8fd95d34152a20
Kyle W. <8kwest@gmail.com>
2025-04-22 21:37:14 -0600

    formatting nearing completion

--- src/components/hike/AddHike.jsx
+++ src/components/hike/AddHike.jsx
@@ -6,7 +6,7 @@ const PlusButton = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="px-3 py-1 rounded-xl hover:bg-black hover:text-white transition w-fit h-fit "
+      className="px-1 py-1 rounded-xl hover:bg-black hover:text-white transition w-fit h-fit "
       aria-label="Add item"
     >
       <span className="text-xl">Log Hike</span>
