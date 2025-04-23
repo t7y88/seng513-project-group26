041c0c70999a97b898af7a6ac84e961d770ad065
Kyle W. <8kwest@gmail.com>
2025-04-22 21:56:00 -0600

    formatting of hikepage

--- src/components/hike/AddHike.jsx
+++ src/components/hike/AddHike.jsx
@@ -6,10 +6,10 @@ const PlusButton = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="px-1 py-1 rounded-xl hover:bg-black hover:text-white transition w-fit h-fit "
+      className="px-1 py-1 rounded-xl hover:bg-black hover:text-white transition w-fit h-fit flex-wrap"
       aria-label="Add item"
     >
-      <span className="text-xl">Log Hike</span>
+      <span className="text-xl flex-wrap">Log Hike</span>
     </button>
   );
 };
